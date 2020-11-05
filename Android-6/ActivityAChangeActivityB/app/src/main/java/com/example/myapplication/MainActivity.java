e613ced11af692763be695090ce2aafa43a25d9c
Arthur <asd87120521@gmail.com>
2020-11-05 14:38:38 +0800

    Android-Studio-6-ActivitySwitch

---
+++ Android-6/ActivityAChangeActivityB/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity",  "onStart:");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("MainActivity", "onRestart:");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume:");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause:");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop:");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy:");
+    }
+
+
+    public void A(View view){
+        Intent intent = new Intent( this,MainActivity2.class);
+        startActivity(intent);
+    }
+}
